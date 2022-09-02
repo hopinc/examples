5da8994cbdad581929f913b68517aab14978d8b7
Alistair Smith <hi@alistair.sh>
2022-09-02 20:02:58 +0200

    feat: add example using deno

---
+++ js/deno-with-sdk/main.ts
@@ -0,0 +1,15 @@
+import { Hop, APIAuthentication } from "https://esm.sh/@onehop/js";
+import * as dotenv from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+
+const { HOP_API_KEY } = dotenv.config({
+	path: import.meta.resolve("./.env"),
+	safe: true,
+});
+
+if (import.meta.main) {
+	const hop = new Hop(HOP_API_KEY as APIAuthentication);
+
+	const deployments = await hop.ignite.deployments.getAll("project_MzMyMzcxMzA5MzUxMTU4MzQ");
+
+	console.log(deployments);
+}
